8f996ac467d91c61761b6fa69f93b086018c7df8
Eduardo Gavilan <eduardogavilan3026@gmail.com>
2024-11-13 17:58:51 -0300

    nvim updated

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
+package.path = "/home/Luna/.config/nvim/?.lua;" .. package.path
 -- Disable nvim built-in "Nerd-tree"
 vim.g.mapleader = " "
 vim.g.loaded_netrw = 1
@@ -49,51 +50,32 @@ require("lazy").setup({
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
     },
-
+    'matsuuu/pinkmare',
+    'Yagua/nebulous.nvim',
     "startup-nvim/startup.nvim",
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
 })
 
-require('nightfox').setup({
-  options = {
-    -- Compiled file's destination location
-    style = "Carbonfox",
-    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-    compile_file_suffix = "_compiled", -- Compiled file suffix
-    transparent = true,     -- Disable setting background
-    terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false,    -- Non focused panes set to alternative background
-    module_default = true,   -- Default enable value for modules
-    styles = {               -- Style to be applied to different syntax groups
-      comments = "NONE",     -- Value is any valid attr-list value `:help attr-list`
-      conditionals = "NONE",
-      constants = "NONE",
-      functions = "NONE",
-      keywords = "NONE",
-      numbers = "NONE",
-      operators = "NONE",
-      strings = "NONE",
-      types = "NONE",
-      variables = "NONE",
-    },
-    inverse = {             -- Inverse highlight for different types
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
-    modules = {             -- List of various plugins and additional options
-      -- ...
-    },
-  },
-  palettes = {},
-  specs = {},
-  groups = {},
-})
+require("themes.nightfox.theme")
 vim.cmd([[colorscheme carbonfox]])
 require('nvim-tree').setup()
 require('leap').create_default_mappings()
 require("autoclose").setup()
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "lua", "rust", "toml", "python" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting=false,
+  },
+  ident = { enable = true }, 
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
 require("mason").setup({
     ui = {
         icons = {
@@ -115,23 +97,10 @@ vim.diagnostic.config({
 })
 vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
 
+require('telescope').setup()
 -- Treesitter Plugin Setup 
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "lua", "rust", "toml", "python" },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting=false,
-  },
-  ident = { enable = true }, 
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  }
-}
 
-
+require("themes.nightfox.tabby_colors")
 
 -- LSP Diagnostics Options Setup 
 local sign = function(opts)
@@ -142,202 +111,7 @@ local sign = function(opts)
   })
 end
 
-local fmt = string.format
-
-----------------------------------------------------------------------------------------------------
--- Colors
-
----Convert color number to hex string
----@param n number
----@return string
-local hex = function(n)
-  if n then
-    return fmt("#%06x", n)
-  end
-end
-
----Parse `style` string into nvim_set_hl options
----@param style string
----@return table
-local function parse_style(style)
-  if not style or style == "NONE" then
-    return {}
-  end
-
-  local result = {}
-  for token in string.gmatch(style, "([^,]+)") do
-    result[token] = true
-  end
-
-  return result
-end
-
----Get highlight opts for a given highlight group name
----@param name string
----@return table
-local function get_highlight(name)
-  local hl = vim.api.nvim_get_hl_by_name(name, true)
-  if hl.link then
-    return get_highlight(hl.link)
-  end
-
-  local result = parse_style(hl.style)
-  result.fg = hl.foreground and hex(hl.foreground)
-  result.bg = hl.background and hex(hl.background)
-  result.sp = hl.special and hex(hl.special)
-
-  return result
-end
-
----Set highlight group from provided table
----@param groups table
-local function set_highlights(groups)
-  for group, opts in pairs(groups) do
-    vim.api.nvim_set_hl(0, group, opts)
-  end
-end
-
----Generate a color palette from the current applied colorscheme
----@return table
-local function generate_pallet_from_colorscheme()
-  -- stylua: ignore
-  local color_map = {
-    black   = { index = 0, default = "#393b44" },
-    red     = { index = 1, default = "#c94f6d" },
-    green   = { index = 2, default = "#81b29a" },
-    yellow  = { index = 3, default = "#dbc074" },
-    blue    = { index = 4, default = "#719cd6" },
-    magenta = { index = 5, default = "#9d79d6" },
-    cyan    = { index = 6, default = "#63cdcf" },
-    white   = { index = 7, default = "#dfdfe0" },
-  }
-
-  local pallet = {}
-  for name, value in pairs(color_map) do
-    local global_name = "terminal_color_" .. value.index
-    pallet[name] = vim.g[global_name] and vim.g[global_name] or value.default
-  end
-
-  pallet.sl = get_highlight("StatusLine")
-  pallet.tab = get_highlight("TabLine")
-  pallet.sel = get_highlight("TabLineSel")
-  pallet.fill = get_highlight("TabLineFill")
-
-  return pallet
-end
-
----Generate user highlight groups based on the curent applied colorscheme
----
----NOTE: This is a global because I dont known where this file will be in your config
----and it is needed for the autocmd below
-_G._genreate_user_tabline_highlights = function()
-  local pal = generate_pallet_from_colorscheme()
-
-  -- stylua: ignore
-  local sl_colors = {
-    Black   = { fg = pal.black,   bg = pal.white },
-    Red     = { fg = pal.red,     bg = pal.sl.bg },
-    Green   = { fg = pal.green,   bg = pal.sl.bg },
-    Yellow  = { fg = pal.yellow,  bg = pal.sl.bg },
-    Blue    = { fg = pal.blue,    bg = pal.sl.bg },
-    Magenta = { fg = pal.magenta, bg = pal.sl.bg },
-    Cyan    = { fg = pal.cyan,    bg = pal.sl.bg },
-    White   = { fg = pal.white,   bg = pal.black },
-  }
-
-  local colors = {}
-  for name, value in pairs(sl_colors) do
-    colors["User" .. name] = { fg = value.fg, bg = value.bg, bold = true }
-    colors["UserRv" .. name] = { fg = value.bg, bg = value.fg, bold = true }
-  end
-
-  local groups = {
-    -- tabline
-    UserTLHead = { fg = pal.fill.bg, bg = pal.cyan },
-    UserTLHeadSep = { fg = pal.cyan, bg = pal.fill.bg },
-    UserTLActive = { fg = pal.sel.fg, bg = pal.sel.bg, bold = true },
-    UserTLActiveSep = { fg = pal.sel.bg, bg = pal.fill.bg },
-    UserTLBoldLine = { fg = pal.tab.fg, bg = pal.tab.bg, bold = true },
-    UserTLLineSep = { fg = pal.tab.bg, bg = pal.fill.bg },
-  }
-
-  set_highlights(vim.tbl_extend("force", colors, groups))
-end
-
-_genreate_user_tabline_highlights()
-
-vim.api.nvim_create_augroup("UserTablineHighlightGroups", { clear = true })
-vim.api.nvim_create_autocmd({ "SessionLoadPost", "ColorScheme" }, {
-  callback = function()
-    _genreate_user_tabline_highlights()
-  end,
-})
-
-----------------------------------------------------------------------------------------------------
--- Feline
-
-local filename = require("tabby.filename")
-
-local cwd = function()
-  return "  " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. " "
-end
-
-local line = {
-  hl = "TabLineFill",
-  layout = "active_wins_at_tail",
-  head = {
-    { cwd, hl = "UserTLHead" },
-    { "", hl = "UserTLHeadSep" },
-  },
-  active_tab = {
-    label = function(tabid)
-      return {
-        "  " .. tabid .. " ",
-        hl = "UserTLActive",
-      }
-    end,
-    left_sep = { "", hl = "UserTLActiveSep" },
-    right_sep = { "", hl = "UserTLActiveSep" },
-  },
-  inactive_tab = {
-    label = function(tabid)
-      return {
-        "  " .. tabid .. " ",
-        hl = "UserTLBoldLine",
-      }
-    end,
-    left_sep = { "", hl = "UserTLLineSep" },
-    right_sep = { "", hl = "UserTLLineSep" },
-  },
-  top_win = {
-    label = function(winid)
-      return {
-        "  " .. filename.unique(winid) .. " ",
-        hl = "TabLine",
-      }
-    end,
-    left_sep = { "", hl = "UserTLLineSep" },
-    right_sep = { "", hl = "UserTLLineSep" },
-  },
-  win = {
-    label = function(winid)
-      return {
-        "  " .. filename.unique(winid) .. " ",
-        hl = "TabLine",
-      }
-    end,
-    left_sep = { "", hl = "UserTLLineSep" },
-    right_sep = { "", hl = "UserTLLineSep" },
-  },
-  tail = {
-    { "", hl = "UserTLHeadSep" },
-    { "  ", hl = "UserTLHead" },
-  },
-}
-
-require("tabby").setup({
-  tabline = line,
-})
+require("tabby_config")
 
 sign({name = 'DiagnosticSignError', text = ''})
 sign({name = 'DiagnosticSignWarn', text = ''})
@@ -411,234 +185,10 @@ cmp.setup({
   },
 })
 
-local lualine = require('lualine')
-
--- Color table for highlights
--- stylua: ignore
-local colors = {
-  bg       = '#202328',
-  fg       = '#bbc2cf',
-  yellow   = '#ECBE7B',
-  cyan     = '#008080',
-  darkblue = '#081633',
-  green    = '#98be65',
-  orange   = '#FF8800',
-  violet   = '#a9a1e1',
-  magenta  = '#c678dd',
-  blue     = '#51afef',
-  red      = '#ec5f67',
-}
-
-local conditions = {
-  buffer_not_empty = function()
-    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-  end,
-  hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
-  end,
-  check_git_workspace = function()
-    local filepath = vim.fn.expand('%:p:h')
-    local gitdir = vim.fn.finddir('.git', filepath .. ';')
-    return gitdir and #gitdir > 0 and #gitdir < #filepath
-  end,
-}
-
--- Config
-local config = {
-  options = {
-    globalstatus = true,
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    -- Disable sections and component separators
-    component_separators = '',
-    section_separators = '',
-    theme = {
-      -- We are going to use lualine_c an lualine_x as left and
-      -- right section. Both are highlighted by c theme .  So we
-      -- are just setting default looks o statusline
-      normal = { c = { fg = colors.fg, bg = colors.bg } },
-      inactive = { c = { fg = colors.fg, bg = colors.bg } },
-    },
-  },
-  sections = {
-    -- these are to remove the defaults
-    lualine_a = {},
-    lualine_b = {},
-    lualine_y = {},
-    lualine_z = {},
-    -- These will be filled later
-    lualine_c = {},
-    lualine_x = {},
-  },
-  inactive_sections = {
-    -- these are to remove the defaults
-    lualine_a = {},
-    lualine_b = {},
-    lualine_y = {},
-    lualine_z = {},
-    lualine_c = {},
-    lualine_x = {},
-  },
-}
-
--- Inserts a component in lualine_c at left section
-local function ins_left(component)
-  table.insert(config.sections.lualine_c, component)
-end
-
--- Inserts a component in lualine_x at right section
-local function ins_right(component)
-  table.insert(config.sections.lualine_x, component)
-end
-
-ins_left {
-  function()
-    return '▊'
-  end,
-  color = { fg = colors.blue }, -- Sets highlighting of component
-  padding = { left = 0, right = 1 }, -- We don't need space before this
-}
-
-ins_left {
-  -- mode component
-  function()
-    return ''
-  end,
-  color = function()
-    -- auto change color according to neovims mode
-    local mode_color = {
-      n = colors.red,
-      i = colors.green,
-      v = colors.blue,
-      ['␖'] = colors.blue,
-      V = colors.blue,
-      c = colors.magenta,
-      no = colors.red,
-      s = colors.orange,
-      S = colors.orange,
-      ['␓'] = colors.orange,
-      ic = colors.yellow,
-      R = colors.violet,
-      Rv = colors.violet,
-      cv = colors.red,
-      ce = colors.red,
-      r = colors.cyan,
-      rm = colors.cyan,
-      ['r?'] = colors.cyan,
-      ['!'] = colors.red,
-      t = colors.red,
-    }
-    return { fg = mode_color[vim.fn.mode()] }
-  end,
-  padding = { right = 1 },
-}
-
-ins_left {
-  -- filesize component
-  'filesize',
-  cond = conditions.buffer_not_empty,
-}
-
-ins_left {
-  'filename',
-  cond = conditions.buffer_not_empty,
-  color = { fg = colors.magenta, gui = 'bold' },
-}
-
-ins_left { 'location' }
-
-ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
-
-ins_left {
-  'diagnostics',
-  sources = { 'nvim_diagnostic' },
-  symbols = { error = ' ', warn = ' ', info = ' ' },
-  diagnostics_color = {
-    color_error = { fg = colors.red },
-    color_warn = { fg = colors.yellow },
-    color_info = { fg = colors.cyan },
-  },
-}
-
--- Insert mid section. You can make any number of sections in neovim :)
--- for lualine it's any number greater then 2
-ins_left {
-  function()
-    return '%='
-  end,
-}
-
-ins_left {
-  -- Lsp server name .
-  function()
-    local msg = 'No Active Lsp'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return msg
-    end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
-      end
-    end
-    return msg
-  end,
-  icon = ' LSP:',
-  color = { fg = '#ffffff', gui = 'bold' },
-}
-
--- Add components to right sections
-ins_right {
-  'o:encoding', -- option component same as &encoding in viml
-  fmt = string.upper, -- I'm not sure why it's upper case either ;)
-  cond = conditions.hide_in_width,
-  color = { fg = colors.green, gui = 'bold' },
-}
-
-ins_right {
-  'fileformat',
-  fmt = string.upper,
-  icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-  color = { fg = colors.green, gui = 'bold' },
-}
-
-ins_right {
-  'branch',
-  icon = '',
-  color = { fg = colors.violet, gui = 'bold' },
-}
-
-ins_right {
-  'diff',
-  -- Is it me or the symbol for modified us really weird
-  symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
-  diff_color = {
-    added = { fg = colors.green },
-    modified = { fg = colors.orange },
-    removed = { fg = colors.red },
-  },
-  cond = conditions.hide_in_width,
-}
-
-ins_right {
-  function()
-    return '▊'
-  end,
-  color = { fg = colors.blue },
-  padding = { left = 1 },
-}
-
--- Now don't forget to initialize lualine
-lualine.setup(config)
-
+require("lualine_conf")
 require("dapui").setup()
 require("startup").setup()
 require("toggleterm").setup()
-
 require("debug")
 local dap = require('dap')
 
@@ -652,6 +202,12 @@ vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<M-l>', ':tabnext<CR>', {noremap= true, silent = true})
 vim.api.nvim_set_keymap('n', '<M-h>', ':tabprevious<CR>', {noremap= true, silent = true})
 vim.api.nvim_set_keymap('n', '<M-p>', ":ToggleTerm<CR>", {noremap= true, silent= true})
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
